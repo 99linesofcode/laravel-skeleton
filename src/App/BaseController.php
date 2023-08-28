53bc0728e7629f34e44a2853bebce6753bfd36b4
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2023-08-28 17:20:48 +0200

    chore: synchronize framework files 9.x .. 10.x

--- src/App/BaseController.php
+++ src/App/BaseController.php
@@ -3,13 +3,11 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
 {
     use AuthorizesRequests;
-    use DispatchesJobs;
     use ValidatesRequests;
 }
