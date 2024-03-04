70fe5e8fdd6eaf344500aee69bed5f792fa17db9
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2024-03-04 15:19:33 +0100

    fix(factory): override default discovery conventions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,14 +2,22 @@
 
 namespace Database\Factories;
 
+use Domain\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Models\User>
  */
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
